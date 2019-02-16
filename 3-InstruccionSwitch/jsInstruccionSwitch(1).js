a6c51c23ad4fc3ee8e9fa93465589d39deac78c8
ECaballero14 <47437091+ECaballero14@users.noreply.github.com>
2019-02-15 22:05:08 -0300

    Primer Commit
    ejerciciios

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -1,9 +1,40 @@
 function mostrar()
+
 {
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
+var mensaje;
 
+switch(mesDelAño)
 
+{
 
+  case 'Enero':
+
+  mensaje="Que comiences bien el año";  
+     
+  break;
+     
+  case 'Marzo':
+
+  mensaje="a clases!!!";
+
+  break; 
+
+  case 'Julio':
+
+  mensaje="Se viene las vacaciones";
+
+  break;
+
+  case 'Diciembre':
+
+  mensaje="Felices Fiestas!!!";
+ 
+  break;
+
+}  
+
+ alert(mensaje);
 
 }//FIN DE LA FUNCIÓN
