9dfbf31eddec3dc6f1429007a0acd11782bd9f20
ECaballero14 <47437091+ECaballero14@users.noreply.github.com>
2019-02-28 22:13:44 -0300

    COMMIT 9
    EJERCICIO

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/nueve.js
@@ -1,4 +1,42 @@
 function mostrar()
 {
 
+var marca,tamano,precio;
+var respuesta="Si";
+var i=1;
+var mayorprecio=0;
+var mayorpreciomarca;
+var mayorpreciotamano=0;
+var menorprecio=0;
+var menorpreciomarca;
+var menorpreciotamano=0;
+
+while(respuesta=="Si")
+
+ {
+   marca=prompt("Ingrese la marca");
+   tamano=prompt("Ingrese el tamaño");
+   tamano=parseInt(tamano);
+   precio=prompt("Ingrese el precio");
+   precio=parseInt(precio);
+
+  } 	  
+
+    if(precio<8000)
+    {
+     menorpreciotamano=tamano;
+     mayorpreciomarca=marca;
+     menorprecio=precio;
+    }
+     
+    else
+    {
+     mayorprecio=precio;
+     mayorpreciomarca=marca;
+     mayorpreciotamano=tamano;
+    }  
+ 
+  alert("El mayor precio es "+mayorprecio+" marca"+mayorpreciomarca);
+ 
+
 }
